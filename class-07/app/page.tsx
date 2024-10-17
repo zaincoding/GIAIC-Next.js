f8ae5663977a7dddd89c75c4db01368576638a8c
zain <zainmd.htm@gmail.com>
2024-10-17 20:21:01 +0500

    Add files

---
+++ class-07/app/page.tsx
@@ -0,0 +1,46 @@
+
+// function Home(){
+
+//   return(
+    
+// <>
+//     <h1  className="bg-slate-700  font-thin	">tailwind css hello World</h1>
+//     <h2 className="bg-red-400	">This is heading 2</h2>
+//     <h3  className="text-red-700  font-black	">This is heading 3</h3>
+//     <p className="text-2xl">This is paragraph</p>
+//     <span  className="font-medium">This is span.</span>
+
+
+//     </>
+//   )
+// }
+
+// export default Home;
+
+
+function Home(){
+
+  return(
+    
+<>
+    <h1  className="bg-slate-700  font-thin	">tailwind css hello World</h1>
+    <h2 className="bg-red-400	">This is heading 2</h2>
+    <h3  className="text-red-700  font-black	">This is heading 3</h3>
+    <p className="bg-blue-500 text-2xl  py-3 pl-5  my-4 mx-4   border-8	border-zinc-500">This is paragraph</p>
+    <span  className="bg-gray-600 font-medium  block">This is span.</span>
+    <button className="p-2  bg-pink-300 m-1 border-yellow-500 border-2 rounded-md	">Login</button>
+
+    </>
+  )
+}
+//border is consist of 3 properties
+//1.thickness
+//2 style eg: solid, lined, dashed.  assignment. doted, dash
+//3. color default color black,   
+export default Home;
+
+
+//porrfolio like website/ study webstandard
+//Homepage: Header,Footer
+//2 3 More section with tailwind
+//Use multiple colors and style-sizes
