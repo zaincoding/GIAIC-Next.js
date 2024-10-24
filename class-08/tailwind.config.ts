3d0d80ca01b0b81f8744c4b88732a68b018f6a62
zain <zainmd.htm@gmail.com>
2024-10-24 19:15:44 +0500

    add public folder & tilwind.config file

---
+++ class-08/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#f6bf38",
+        seconday: "#ff6600",
+        fahad: "#00ffff"
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
