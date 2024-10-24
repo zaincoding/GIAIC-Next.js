979a30bec56c1927abafd90b565194b7b04bf61e
zain <zainmd.htm@gmail.com>
2024-10-24 11:55:25 +0500

    Add class 8th work

---
+++ class-08/app/page.tsx
@@ -0,0 +1,81 @@
+
+
+import Image from "next/image";
+
+
+//bg-no-repeat
+//them customization
+
+//rm -rf .next
+
+// export default function Home() {
+//   return (
+
+   //   <div  className="bg-[url('/images/download.PNG')] h-[400px] bg-no-repeat w-full bg-cover">
+   //    {/* <h2  className="pl-[200px] my-[10px] text-[36px]  bg-blue-500">Hello world</h2> */}
+   //    {/* <h2  className="pl-[200px] my-[10px] text-[36px] bg-[#3b82f6] "  >Hello world</h2> */}
+
+
+   //    {/* <h2  className="pl-[200px] my-[10%] text-[36px] bg-[#3b82f6]  p-1">Hello world</h2> */}
+
+   //   </div>
+//   )
+//   }
+
+  //directory/public folder/images/img.jpg   (create public folder outside of app folder.)
+
+  export default function Home() {
+   return (
+      <div className="bg-primary">
+              <h2  className="pl-[200px] my-[10px] text-[36px] text-seconday ">Hello world</h2> 
+              <h2  className="py-[10%] my-[10px] text-[36px]  text-fahad ">Hello world</h2> 
+
+
+   </div>
+   )
+
+}
+
+//design system
+
+
+//................local "git" set user and email.....................//
+// git-scm.comdownload
+
+//create account
+// git config --global user.name "Fahad Khan"   (it will add user name)
+
+//chek that user is add or not
+// git config --global user.name
+
+
+// add email
+//git config --global user.email "raofahad@gmail.com"
+
+//check that email is add or not
+// git config --gloabal user.email
+
+
+//to initialize repo
+
+//git init
+
+
+//stage all the changes
+//git add .
+
+//statusn
+//git status
+
+//git add .
+
+//git status
+
+//git commit -m "Class code updated"
+
+//git status
+
+
+
+
+
